34fcb435fd6bef6b1ba25d4606fe1c4e1d88b293
aupamaka <archana.x.upamaka@centurylink.com>
2020-09-24 21:45:44 +0000

    Lab 4

--- main.js
+++ main.js
@@ -22,7 +22,19 @@ const path = require('path');
  */
 const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
 
-/**
+
+
+class IpAddress {
+  constructor() {
+    // IAP's global log object is used to output errors, warnings, and other
+    // information to the console, IAP's log files, or a Syslog server.
+    // For more information, consult the Log Class guide on the Itential
+    // Developer Hub https://developer.itential.io/ located
+    // under Documentation -> Developer Guides -> Log Class Guide
+    log.info('Starting the IpAddress product.');
+  }
+
+  /**
  * Calculate and return the first host IP address from a CIDR subnet.
  * @param {string} cidrStr - The IPv4 subnet expressed
  *                 in CIDR format.
@@ -70,16 +82,6 @@ getFirstIpAddress(cidrStr, callback) {
   // data as the second argument to the callback function.
   return callback(combinedValue, callbackError);
 }
-
-class IpAddress {
-  constructor() {
-    // IAP's global log object is used to output errors, warnings, and other
-    // information to the console, IAP's log files, or a Syslog server.
-    // For more information, consult the Log Class guide on the Itential
-    // Developer Hub https://developer.itential.io/ located
-    // under Documentation -> Developer Guides -> Log Class Guide
-    log.info('Starting the IpAddress product.');
-  }
 }
 
 module.exports = new IpAddress;
